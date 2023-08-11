bada9deeab9d111622b1c31adcda40d495baaf98
Andres <andres.m.bosques@gmail.com>
2023-08-11 11:58:30 -0600

    first attempt

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import socket
 from realsense_depth import *
+import threading
 
 def getDistance(points,frame, prev_dist = None):
     """Obtain the location of the middle point of the registered hand
@@ -24,8 +25,19 @@ def getDistance(points,frame, prev_dist = None):
 
     dist = int((frame[x,y])/10)
     return dist,[x,y],prev_dist
-    
-def setSocket():
+
+def socketList(n):
+    #List of udp connections
+    SKs =[]
+    SAPs = []
+    #Assign each a different port to comunicate to
+    for i in range(n):
+        sk, serverAddPort = setSocket(5001+i)
+        SKs.append(sk)
+        SAPs.append(serverAddPort)
+    return SKs,SAPs
+
+def setSocket(port):
     """Create a UDP Socket conection to transmit data to unity
 
     Returns:
@@ -33,25 +45,42 @@ def setSocket():
     """
     sk = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     #port to send to (ip, port)
-    serverPort = ("127.0.0.1", 8661)
+    serverPort = ("127.0.0.1", port)
     return sk,serverPort
 
-def sendData():
+def sendData(hand,sk,data,depth_frame,height,SAP):
+    
+    #get landmark list
+    lmlst = hand["lmList"]
+    loc = hand["center"]
+    
+    #get the estimated distence of the center of the hand from the camera            
+    dist = int((depth_frame[loc[1],loc[0]])/1)
+    
+    for lm in lmlst:
+        #Save the data with the oposite value since unity saves data oposite to open cv
+        data.extend([lm[0],height - lm[1],lm[2],dist])
+
+    sk.sendto(str.encode(str(data)), SAP)
+
+
+def main(n):
     #Parameters
     height = 1200
 
-    #udp info
-    sk, serverAddPort = setSocket()
+    #Obtain list of sockets and server add ports
+    sockets,saps = socketList(n)
 
     #webcam
-    #Bootup the camera
     dc = DepthCamera()
 
-
     #hand detector
-    detector = HandDetector(maxHands=1, detectionCon=0.8)
+    detector = HandDetector(maxHands=n, detectionCon=0.8)
 
-    prev_dist = None
+    #A list for each hand data given to a seperate thread
+    data=[[],[],[],[]]
+
+    threads = []
 
     while True:
         #get the frame
@@ -59,34 +88,27 @@ def sendData():
 
         #get the hands
         hands, color_frame = detector.findHands(color_frame)
-
-        data = []
         
 
         #land mark values = (x,y,z, overall depth) * 21 (total number of values we have)
         if hands:
-            #get the first hand detected
-            hand = hands[0]
-            
-            #get landmark list
-            lmlst = hand["lmList"]
-            loc = hand["center"]
-            
-            #get the estimated distence of the center of the hand from the camera            
-            dist = int((depth_frame[loc[1],loc[0]])/1)
-
-            for lm in lmlst:
-                #Save the data with the oposite value since unity saves data oposite to open cv
-                data.extend([lm[0],height - lm[1],lm[2],dist])
-
-            sk.sendto(str.encode(str(data)), serverAddPort)
-
+            for i in hands:
+                threads.append(threading.Thread(target = sendData,
+                                                args = (hands[i],sockets[i],
+                                                        data[i],depth_frame,height,saps[i])))
+                threads[i].start()
             #cv2.circle(color_frame, loc, 4, (255,0,0))
             #cv2.putText(color_frame, "{}cm".format(dist), (loc), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
+        
         cv2.imshow("Image", color_frame)
         key = cv2.waitKey(1)
         if key == 32:
+
+            if threads:
+                for i in range(len(threads)):
+                    threads[i].join()
+            
             break
 
 if __name__ == "__main__":
-    sendData()
+    main(2)
