5ed016ccd115c9c43f8bf652ab312d08ff6eec40
Andres <andres.m.bosques@gmail.com>
2023-08-15 09:31:27 -0600

    clean up

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
+import pyrealsense2 as rs
+import numpy as np
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 import socket
-from realsense_depth import *
 import threading
+
+class DepthCamera:
+    def __init__(self):
+        # Configure depth and color streams
+        self.pipeline = rs.pipeline()
+        config = rs.config()
+
+        # Get device product line for setting a supporting resolution
+        pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+        pipeline_profile = config.resolve(pipeline_wrapper)
+        device = pipeline_profile.get_device()
+        device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+
+
+        # Start streaming
+        self.pipeline.start(config)
+
+    def get_frame(self):
+        frames = self.pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+
+        depth_image = np.asanyarray(depth_frame.get_data())
+        color_image = np.asanyarray(color_frame.get_data())
+        if not depth_frame or not color_frame:
+            return False, None, None
+        return True, depth_image, color_image
+
+    def release(self):
+        self.pipeline.stop()
 
 def getDistance(points,frame, prev_dist = None):
     """Obtain the location of the middle point of the registered hand
@@ -63,7 +98,6 @@ def sendData(hand,sk,depth_frame,height,SAP):
 
     sk.sendto(str.encode(str(data)), SAP)
     #print(f"-----\nsent: {data}\nto:{SAP}")
-
 
 def main(n):
     #Parameters
