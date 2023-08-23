1d9472696809033792ec5f7d3a1eacd0bbcccfd5
Mancorb <andres.m.bosques@gmail.com>
2023-08-23 16:06:56 -0600

    Image recognition test_1
    Failed

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ class DepthCamera:
         pipeline_profile = config.resolve(pipeline_wrapper)
         device = pipeline_profile.get_device()
 
-        config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-        config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+        config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
         # Start streaming
         self.pipeline.start(config)
