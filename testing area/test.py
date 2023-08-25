45b4b064362fe90125043f1f35df099d6e2055d2
Mancorb <andres.m.bosques@gmail.com>
2023-08-25 15:53:45 -0600

    Successfull test closed fingers (Thumb pending)
    The program detects if the fingers are closed (except for thumbs)

--- testing area/test.py
+++ testing area/test.py
@@ -1,19 +1,43 @@
 import cv2
 import HandTrackingModule as htm
+from os import system
 
 def middle(x1,x2,y1,y2):
+    """Estimate the middle point between two points
+
+    Args:
+        x1 (int): locaiton x of the first point
+        x2 (int): location x of the second point
+        y1 (int): location y of the first point
+        y2 (int): location y of the second point
+
+    Returns:
+        tuple: the estimated x and y location of the middle point.
+    """
     x = int((x1+x2)/2)
     y = int((y1+y2)/2)
     return x,y
 
 def isInside(circle_x, circle_y, rad, x, y):
+    """Find of a point is inside an estimated circle
+
+    Args:
+        circle_x (int): location x of the circle's center
+        circle_y (int): location y of the circle's center
+        rad (int): raius of the circle in pixels
+        x (int): location of the point to find
+        y (int): location y of the point to find
+
+    Returns:
+        bool: If the point is inside the circle then it will return True, else it will return False
+    """
     if ((x - circle_x) * (x - circle_x) +
         (y - circle_y) * (y - circle_y) <= rad * rad):
         return True
     else:
         return False
 
-def indexStraight(x1,x2,y1,y2,tx,ty):
+def FingerStraight(x1,x2,y1,y2,tx,ty):
     """Make a circle located between the location of the beggining of the index finger and the tip if the middle bone
     of the finger is within the radius of the circle, but neither the tip nor the bottom are in the range then we know 
     the finger is straight. If so then it will draw a circle in the
@@ -40,32 +64,38 @@ def indexStraight(x1,x2,y1,y2,tx,ty):
 wCam, hCam= 1280, 720
 
 cap = cv2.VideoCapture(1)
-
-
 cap.set(3,wCam)
 cap.set(4,hCam)
 
 detector = htm.handDetector(detectionCon= 1)
+            #(tip, bottom, middle)
+finger_loc = [(8,5,6),(12,9,10),(16,13,14),(20,17,18)]
+# for the thumb just check if the middle part of the thumb is near the base of the index finger
 
 while True:
-    fingers = {"index":False,"middle":False,"ring":False,"pinky":False, "thumb":False}
+    fingers = [False,False,False,False,False]
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img,draw=False)
+    counter = 0
     
     if len(lmList)!=0:
-        x1,x2 =lmList[8][1],lmList[5][1]
-        y1,y2 =lmList[8][2],lmList[5][2]
-        tx,ty =lmList[6][1],lmList[6][2]
+        for i in finger_loc:
+            #[(tip, bottom, middle)]
+            x1,x2 =lmList[i[0]][1],lmList[i[1]][1]
+            y1,y2 =lmList[i[0]][2],lmList[i[1]][2]
+            tx,ty =lmList[i[2]][1],lmList[i[2]][2]
 
-        if indexStraight(x1,x2,y1,y2,tx,ty):
-            img = cv2.circle(img, middle(x1,x2,y1,y2), radius=20, color=(0, 0, 255), thickness=1)
-        
-
+            if FingerStraight(x1,x2,y1,y2,tx,ty):
+                img = cv2.circle(img, middle(x1,x2,y1,y2), radius=20, color=(0, 0, 255), thickness=1)
+                fingers[counter] = True
+                
+            counter += 1
 
     cv2.imshow("Image", img)
 
-    print(f"\rIndex:{fingers['index']}\tMiddle:{fingers['middle']}\tRing:{fingers['ring']}\tPinky:{fingers['pinky']}\tThumb:{fingers['thumb']}",end = "")
+    print(fingers)
+    system("cls")
     
     if cv2.waitKey(1) == 32:
         break
