96e82195ffb8ae9b8508aeab90af623776dd82a2
Mancorb <andres.m.bosques@gmail.com>
2023-09-05 15:13:28 -0600

    Esthetics - 02
    Changed names and location of methods in Finger_detector

--- Finger_detector.py
+++ Finger_detector.py
@@ -1,85 +1,6 @@
 from math import sqrt,pow
 
 class Finger_detector:
-
-    def fingerStraight(self, x1:int,x2:int,y1:int,y2:int,tx:int,ty:int) -> bool:
-        """
-        Obtiene el punto medio entre la localización de la punta del Metacarpo y la punta de la falange intermedia.
-        Dibuja un círculo de un radio determinado en dicha localización.
-        Si la punta de la falange proximal está entre el radio del círculo , pero la falange intermedia y el metacarpo
-        no están dentro del círculo entonces se sabe que el dedo está derecho.
-        En dicho caso, será dibujado un círculo.
-    
-        Args:
-            x1 (int): Localización de X de la base del dedo
-            x2 (int): Localización de X de la punta del dedo
-            y1 (int): Localización de Y de la base del dedo 
-            y2 (int): Localización de Y de la punto del dedo 
-            tx (int): Localización de X del hueso medio del dedo 
-            ty (int): Localización de Y del hueso medio del dedo 
-
-        Returns:
-            bool: True si el dedo se encuentra derecho, False si el dedo no está derecho
-        """
-        rad = 20
-        x,y= self.middle(x1,x2,y1,y2)
-        mid = self.isInside(x,y,rad,tx,ty)
-        base = self.isInside(x,y,rad,x1,y1)
-        tip = self.isInside(x,y,rad,x2,y2)
-        
-        if mid and not base and not tip:
-            return True
-        else : return False
-
-    
-    def thumb_contracted(self, hand_x, hand_y, thumb_x, thumb_y) -> bool:
-        """
-        Dibuja un círculo entre la mano y el pulgar para detectar si el pulgar está extendido o no
-
-        Args:
-            hand_x (int): Localización de X de la mano
-            hand_y (int): Localización de Y de la mano
-            thumb_x (int): Localización de X de la parte media del pulgar
-            thumb_y (int): Localización de Y de la parte media del pulgar
-
-        Returns:
-            bool: Retorna True si el pulgar no está extendido, False en caso contrario 
-        """
-        rad = 35
-        x,y = self.middle(hand_x,thumb_x,hand_y,thumb_y)
-        return not self.isInside(x,y,rad,thumb_x,thumb_y)
-
-
-    def detect_pinch(self, x1:int,y1:int,x2:int,y2:int,x3:int,y3:int, index:bool, thumb:bool) -> bool:
-        """
-        Detecta si la mano está haciendo el gesto similar al de pellizco 
-        basándose en la distancia entre el pulgar y el dedo índice
-    
-        Args:
-            x1 (int): Localización de X de la punta del pulgar
-            y1 (int): Localización de Y de la punta del pulgar
-            x2 (int): Localización de X de la punta del índice
-            y2 (int): Localización de Y de la punta del índice
-            x3 (int): Localización de X del punto medio del pulgar
-            y3 (int): Localización de X del punto medio del pulgar
-            index (bool): True, si el dedo está extendido
-            thumb (bool): True, si el pulgar no está contraido
-    
-        Returns:
-            bool: True, si el usuario está haciendo el gesto de pellizco
-        """
-    
-        #si el índice está contraido y el pulgar no
-        if (not index and thumb):
-            d1 = self.dist_2_points(x1,x3,y1,y3) #Distancia entre punto 4 y 3
-            d2 = self.dist_2_points(x1,x2,y1,y2) #Distancia entre punto 4 y 8
-    
-            if d1>d2:
-                return True
-        
-        return False
-
-    
     def detect(self, lmList) -> tuple:
         """
         Retorna una tupla, el 1er elemento es una lista de booleanos indicando si un dedo está contraido 
@@ -110,7 +31,7 @@ class Finger_detector:
                 y1,y2 =lmList[i[0]][2],lmList[i[1]][2]
                 tx,ty =lmList[i[2]][1],lmList[i[2]][2]
     
-                fingers[counter] = self.fingerStraight(x1,x2,y1,y2,tx,ty)
+                fingers[counter] = self._fingerStraight(x1,x2,y1,y2,tx,ty)
     
                 counter += 1
             
@@ -118,9 +39,9 @@ class Finger_detector:
             hand_x, thumb_x=lmList[5][1],lmList[3][1] #Localización de X del punto 5,localización de X del punto 3
             hand_y, thumb_y=lmList[5][2],lmList[3][2] #Lo mismo, pero la localización en Y
                 
-            fingers[-1] = self.thumb_contracted(hand_x,hand_y,thumb_x,thumb_y) #Asigna el resultado a un arreglo
+            fingers[-1] = self._thumb_contracted(hand_x,hand_y,thumb_x,thumb_y) #Asigna el resultado a un arreglo
                 
-            pinch = self.detect_pinch(lmList[4][1],lmList[4][2],
+            pinch = self._detect_pinch(lmList[4][1],lmList[4][2],
                                  lmList[8][1],lmList[8][2],
                                  lmList[3][1],lmList[3][2],
                                  fingers[0],
@@ -128,8 +49,86 @@ class Finger_detector:
             
         return (fingers,pinch)
 
+
+    def _fingerStraight(self, x1:int,x2:int,y1:int,y2:int,tx:int,ty:int) -> bool:
+        """
+        Obtiene el punto medio entre la localización de la punta del Metacarpo y la punta de la falange intermedia.
+        Dibuja un círculo de un radio determinado en dicha localización.
+        Si la punta de la falange proximal está entre el radio del círculo , pero la falange intermedia y el metacarpo
+        no están dentro del círculo entonces se sabe que el dedo está derecho.
+        En dicho caso, será dibujado un círculo.
     
-    def middle(self,x1:int,x2:int,y1:int,y2:int) -> tuple:
+        Args:
+            x1 (int): Localización de X de la base del dedo
+            x2 (int): Localización de X de la punta del dedo
+            y1 (int): Localización de Y de la base del dedo 
+            y2 (int): Localización de Y de la punto del dedo 
+            tx (int): Localización de X del hueso medio del dedo 
+            ty (int): Localización de Y del hueso medio del dedo 
+
+        Returns:
+            bool: True si el dedo se encuentra derecho, False si el dedo no está derecho
+        """
+        rad = 20
+        x,y= self._middle(x1,x2,y1,y2)
+        mid = self._isInside(x,y,rad,tx,ty)
+        base = self._isInside(x,y,rad,x1,y1)
+        tip = self._isInside(x,y,rad,x2,y2)
+        
+        if mid and not base and not tip:
+            return True
+        else : return False
+
+    
+    def _thumb_contracted(self, hand_x, hand_y, thumb_x, thumb_y) -> bool:
+        """
+        Dibuja un círculo entre la mano y el pulgar para detectar si el pulgar está extendido o no
+
+        Args:
+            hand_x (int): Localización de X de la mano
+            hand_y (int): Localización de Y de la mano
+            thumb_x (int): Localización de X de la parte media del pulgar
+            thumb_y (int): Localización de Y de la parte media del pulgar
+
+        Returns:
+            bool: Retorna True si el pulgar no está extendido, False en caso contrario 
+        """
+        rad = 35
+        x,y = self._middle(hand_x,thumb_x,hand_y,thumb_y)
+        return not self._isInside(x,y,rad,thumb_x,thumb_y)
+
+
+    def _detect_pinch(self, x1:int,y1:int,x2:int,y2:int,x3:int,y3:int, index:bool, thumb:bool) -> bool:
+        """
+        Detecta si la mano está haciendo el gesto similar al de pellizco 
+        basándose en la distancia entre el pulgar y el dedo índice
+    
+        Args:
+            x1 (int): Localización de X de la punta del pulgar
+            y1 (int): Localización de Y de la punta del pulgar
+            x2 (int): Localización de X de la punta del índice
+            y2 (int): Localización de Y de la punta del índice
+            x3 (int): Localización de X del punto medio del pulgar
+            y3 (int): Localización de X del punto medio del pulgar
+            index (bool): True, si el dedo está extendido
+            thumb (bool): True, si el pulgar no está contraido
+    
+        Returns:
+            bool: True, si el usuario está haciendo el gesto de pellizco
+        """
+    
+        #si el índice está contraido y el pulgar no
+        if (not index and thumb):
+            d1 = self._dist_2_points(x1,x3,y1,y3) #Distancia entre punto 4 y 3
+            d2 = self._dist_2_points(x1,x2,y1,y2) #Distancia entre punto 4 y 8
+    
+            if d1>d2:
+                return True
+        
+        return False
+
+    
+    def _middle(self,x1:int,x2:int,y1:int,y2:int) -> tuple:
         """Estimar el punto medio entre dos puntos
     
         Args:
@@ -146,7 +145,7 @@ class Finger_detector:
         return x,y
 
     
-    def isInside(self,circle_x:int, circle_y:int, rad:int, x:int, y:int) -> bool:
+    def _isInside(self,circle_x:int, circle_y:int, rad:int, x:int, y:int) -> bool:
         """Encuentra el punto que está dentro de un círculo estimado
     
         Args:
@@ -166,7 +165,7 @@ class Finger_detector:
             return False
 
     
-    def dist_2_points(self,x1:int, x2:int, y1:int, y2:int) -> int:
+    def _dist_2_points(self,x1:int, x2:int, y1:int, y2:int) -> int:
         """Encuentra la distancia entre 2 puntos
     
         Args:
@@ -181,4 +180,7 @@ class Finger_detector:
         # _/ (x2-x1)*2 + (y2-y1)*2
         return sqrt(pow((x2-x1),2) + pow((y2-y1),2))
 
+
+    #detector = htm.handDetector(detectionCon= 1)
+    #finger_detector(finger_loc,detector)
     
