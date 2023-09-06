631d7168206397b9aef7b0d7ecf1267b92fd5a40
Mancorb <andres.m.bosques@gmail.com>
2023-09-06 15:16:42 -0600

    v 1.0.0
    landmark detector, first succesosfull iteration.

--- MediapipeTest.py
+++ MediapipeTest.py
@@ -2,38 +2,41 @@ import mediapipe as mp
 import cv2
 
 def _get_locations_list (image,detector) ->list:
+    """Method that returns the location of found landmarks
+
+    Args:
+        image (numpy.ndarray): image captured by the camara
+        detector (mediapipe.python.solutions.hands.Hands): Information from the detection algorithim
+
+    Returns:
+        list: List of landmarks
+    """
+    #hight and width of the image
     height = image.shape[0]
     width = image.shape[1]
-    results = detector.process(img)
-    hands_data=[]
+    results = detector.process(image)
+    hands_data=[]#list of landmark lists
 
-    if results.multi_hand_landmarks:
+    if results.multi_hand_landmarks:#if a hand is found
         for hand_no, hand_landmarks in enumerate(results.multi_hand_landmarks):#for each hand found
-            for i in range(21):
-                res = hand_landmarks.landmark[mp_hands.HandLandmark(i).value]
+            temp = []
+            for i in range(21):#obtain ONLY x and Y locations
+                res = hand_landmarks.landmark[detector.HandLandmark(i).value]
 
+                #number is multiplied by the width and hight to get the location within the image
                 x = int(res.x*width)
                 y = int(res.y*height)
 
-                hands_data.append([x,y])
+                temp.append([x,y])
+            hands_data.append(temp)
     
     return hands_data
 
 
-video = cv2.VideoCapture(1)
+""" video = cv2.VideoCapture(1)
 mp_hands = mp.solutions.hands
 detector = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.8)
 
 while True:
     img  = video.read()[1]
-    hands_landmarks =_get_locations_list(img,detector)
-    
-    if len(hands_landmarks)>0:
-        for i in hands_landmarks:
-            img = cv2.circle(img,i,10,(0,255,0),1)
-            
-    cv2.imshow("image",img)
-    if cv2.waitKey(1) == 27: 
-            break
-
-            
+    hands_landmarks =_get_locations_list(img,detector) """
