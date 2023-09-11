69acd10c94a2f2645efb376e3bfe54139d4b64e2
Mancorb <andres.m.bosques@gmail.com>
2023-09-11 10:17:53 -0600

    Development #1
    Added a delete file program to clen all the registered txt files (usefull for commits, and at the end of the program)

---
+++ deleteFiles.py
@@ -0,0 +1,18 @@
+from os import listdir,path,remove
+
+def delete_files_in_directory(directory_path):
+   try:
+     files = listdir(directory_path)
+     for file in files:
+       file_path = path.join(directory_path, file)
+       if path.isfile(file_path):
+        remove(file_path)
+
+   except OSError:
+     print("Error occurred while deleting files.")
+
+if __name__ == "__main__":
+  directory_path_list = ["A","B","C","D"]
+  for letter in directory_path_list:
+      delete_files_in_directory("hand_"+letter)
+  print("All files deleted successfully.")
